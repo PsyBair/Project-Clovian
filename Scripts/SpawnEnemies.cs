09ec529820a1603dc74659b7a54580c891e546a6
Gage Lorenzo Bair <68452033+PsyBair@users.noreply.github.com>
2021-11-01 20:24:45 -0400

    Add files via upload

---
+++ Scripts/SpawnEnemies.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public float respawnTime;
+    private Vector2 screenBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        StartCoroutine(enemyWave());
+    }
+
+    private void spawnEnemy()
+    {
+        GameObject enemy = Instantiate(enemyPrefab) as GameObject;
+        enemy.transform.position = new Vector2(screenBounds.x, Random.Range(-screenBounds.y, screenBounds.y));
+    }
+
+    IEnumerator enemyWave()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            spawnEnemy();
+        }
+    }
+}
