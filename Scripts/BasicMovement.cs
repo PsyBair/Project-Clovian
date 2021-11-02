d7175168724c117f5814a70acf8f93bcff50ec1f
Gage Lorenzo Bair <68452033+PsyBair@users.noreply.github.com>
2021-11-01 23:41:36 -0400

    Add files via upload

---
+++ Scripts/BasicMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    public float speed;
+    public Animator animator;
+    public GameObject arrowPrefab;
+    public float fireRate;
+    private float nextFire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+
+        if(Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            GameObject fireball = Instantiate(arrowPrefab, transform.position, Quaternion.identity);
+            fireball.GetComponent<Rigidbody2D>().velocity = new Vector2(11.0f, 0.0f);
+        }
+
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+        transform.position = transform.position + movement * speed * Time.deltaTime;
+    }
+}
