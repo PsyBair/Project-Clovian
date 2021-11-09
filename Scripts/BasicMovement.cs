231a8c92ee14b5c3a9c8fecaa1e7fd1515bd236a
Gage Lorenzo Bair <68452033+PsyBair@users.noreply.github.com>
2021-11-08 22:15:36 -0500

    Add files via upload

---
+++ Scripts/BasicMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    public float speed;
+    public Animator animator;
+    public GameObject arrowPrefab;
+    public float fireRate;
+    private float nextFire;
+    public float fireDespawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+
+        if(Input.GetKey(KeyCode.Space) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            GameObject fireball = Instantiate(arrowPrefab, transform.position, Quaternion.identity);
+            fireball.GetComponent<Rigidbody2D>().velocity = new Vector2(11.0f, 0.0f);
+            GameObject.Destroy(fireball, fireDespawn);
+        }
+        
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+        transform.position = transform.position + movement * speed * Time.deltaTime;
+    }
+}
---
+++ Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    private new Rigidbody2D rigidbody;
+    private Vector2 screenBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = this.GetComponent<Rigidbody2D>();
+        rigidbody.velocity = new Vector2(-speed, 0.0f);
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -screenBounds.x)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D (Collision2D coll)
+    {
+        if (coll.gameObject.tag.Equals("Shot"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
